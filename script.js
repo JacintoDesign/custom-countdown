cef1e7d8b8f625bbf22dd910766ef5609dc5e0a2
JACINTO WONG <jacinto.wong@cbc.ca>
2020-02-28 12:48:17 -0500

    functional, able to add up to 3 countdowns

--- script.js
+++ script.js
@@ -3,30 +3,65 @@ const second = 1000,
       hour = minute * 60,
       day = hour * 24;
 
-let countdownDate1 = 'Jan 1, 2021';
-let countdownTitle1 = 'New Years Day';
-let countDown1 = new Date(countdownDate1).getTime();
+// let countdownDate1 = 'Jan 1, 2021';
+// let countdownTitle1 = 'New Years Day';
+// let countDown1 = new Date(countdownDate1).getTime();
 
-let countdownDate2 = 'Jan 18, 2022';
-let countdownTitle2 = 'My 35th Birthday';
-let countDown2 = new Date(countdownDate2).getTime();
+// let countdownDate2 = 'Jan 18, 2022';
+// let countdownTitle2 = 'My 35th Birthday';
+// let countDown2 = new Date(countdownDate2).getTime();
 
-let countdownDate3 = 'Jan 18, 2037';
-let countdownTitle3 = 'My 50th Birthday';
-let countDown3 = new Date(countdownDate3).getTime();
+// let countdownDate3 = 'Jan 18, 2037';
+// let countdownTitle3 = 'My 50th Birthday';
+// let countDown3 = new Date(countdownDate3).getTime();
 
-const countdownForm = document.getElementById('countdownForm');
+const countdownForm1 = document.getElementById('countdownForm1');
+const countdownForm2 = document.getElementById('countdownForm2');
+const countdownForm3 = document.getElementById('countdownForm3');
 const countdown1El = document.getElementById('countdown1');
 const countdown2El = document.getElementById('countdown2');
 const countdown3El = document.getElementById('countdown3');
 
-countdownForm.addEventListener('submit', updateCountdown);
+countdownForm1.addEventListener('submit', updateCountdown1);
+countdownForm2.addEventListener('submit', updateCountdown2);
+countdownForm3.addEventListener('submit', updateCountdown3);
 
-function updateCountdown(e) {
+function updateCountdown1(e) {
   e.preventDefault();
-  countdownDate = date.value;
-  countDown = new Date(countdownDate).getTime();
-  updateDOM();
+  countdownTitle1 = title.value;
+  countdownDate1 = date.value;
+  countDown1 = new Date(countdownDate1).getTime();
+  updateDOM(countDown1, countdownTitle1, countdown1El);
+  setTimeout(() => {
+    countdownForm2.innerHTML = `
+    <input type="text" name="Title" id="title" placeholder="Enter Title Here">
+    <input type="text" name="Date" id="date" placeholder="Jan 18, 2021">
+    <button type="submit">Submit</button>
+    `
+  }, 1000);
+}
+
+function updateCountdown2(e) {
+  e.preventDefault();
+  countdownTitle2 = title.value;
+  countdownDate2 = date.value;
+  countDown2 = new Date(countdownDate2).getTime();
+  updateDOM(countDown2, countdownTitle2, countdown2El);
+  setTimeout(() => {
+    countdownForm3.innerHTML = `
+    <input type="text" name="Title" id="title" placeholder="Enter Title Here">
+    <input type="text" name="Date" id="date" placeholder="Jan 18, 2021">
+    <button type="submit">Submit</button>
+    `
+  }, 1000);
+}
+
+function updateCountdown3(e) {
+  e.preventDefault();
+  countdownTitle3 = title.value;
+  countdownDate3 = date.value;
+  countDown3 = new Date(countdownDate3).getTime();
+  updateDOM(countDown3, countdownTitle3, countdown3El);
 }
 
 function updateDOM(countDown, countdownTitle, countdownEl) {
@@ -54,7 +89,3 @@ function updateDOM(countDown, countdownTitle, countdownEl) {
 
   }, second)
 }
-
-updateDOM(countDown1, countdownTitle1, countdown1El);
-updateDOM(countDown2, countdownTitle2, countdown2El);
-updateDOM(countDown3, countdownTitle3, countdown3El);
